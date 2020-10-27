0226bbd9046b5b0e7a4cd32619bc4061fad53bda
cindyxinyiwang <xwang29@nd.edu>
2020-10-27 13:26:49 -0400

    update scripts

--- scripts/average_f1.py
+++ scripts/average_f1.py
@@ -50,7 +50,7 @@ def classify_results(filename):
                     toks = line.split("=")
                     lan = toks[0]
                     acc = toks[1]
-                    results[lan] = float(acc)
+                    results[lan] = float(acc)*100
     
     #print(results)
     #print(sum(results.values()) / len(results))
@@ -94,12 +94,48 @@ def qa_results(filename):
   exact_df = pd.DataFrame(seeded_exact_results)
   combined_df = pd.concat([df, exact_df], keys=('f1','exact f1'))
   print(combined_df)
-  print("ave f1 over random seeds:", combined_df.xs('f1').mean(axis=1).mean())
-  print("ave exact f1 over random seeds:", combined_df.xs('exact f1').mean(axis=1).mean())
+  print("xquad ave f1 over random seeds:", combined_df.xs('f1').mean(axis=1).mean())
+  print("xquad ave exact f1 over random seeds:", combined_df.xs('exact f1').mean(axis=1).mean())
   
   #print(df.mean(axis=1))
   #print(pd.DataFrame(df, columns=langs).mean(axis=0).mean())
   combined_df.to_csv(filename+"_xquad.csv")
+
+  seeded_results = []
+  seeded_exact_results = []
+  for seed in seeds:
+    results = {}
+    exact_results = {}
+    c_f = filename + "_s{}/predictions/mlqa/test_results.txt".format(seed)
+    if os.path.exists(c_f):
+        with open(c_f, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith("language"):
+                    lan = line.split(" = ")[1]
+                elif line.startswith("f1"):
+                    f1 = line.split(" = ")[1]
+                    results[lan] = float(f1)
+                elif line.startswith("exact"):
+                    f1 = line.split(" = ")[1]
+                    exact_results[lan] = float(f1)
+    
+    #print(results)
+    #print(sum(results.values()) / len(results))
+    seeded_results.append(results)
+    seeded_exact_results.append(exact_results)
+  
+  df = pd.DataFrame(seeded_results)
+  exact_df = pd.DataFrame(seeded_exact_results)
+  combined_df = pd.concat([df, exact_df], keys=('f1','exact f1'))
+  print(combined_df)
+  print("mlqa ave f1 over random seeds:", combined_df.xs('f1').mean(axis=1).mean())
+  print("mlqa ave exact f1 over random seeds:", combined_df.xs('exact f1').mean(axis=1).mean())
+  
+  #print(df.mean(axis=1))
+  #print(pd.DataFrame(df, columns=langs).mean(axis=0).mean())
+  combined_df.to_csv(filename+"_mlqa.csv")
+
 
 
 if __name__ == "__main__":
